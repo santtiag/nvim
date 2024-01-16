abb846e1d3449090bf7ac74a028c63d90a8370c8
Santiago <romerosantiago5002@gmail.com>
2024-01-16 16:10:17 -0500

    Addition of all plug in and configuration of the keymaps

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,57 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.cmd('set number')
+vim.cmd('set relativenumber')
+vim.cmd('set cmdheight=0')
+
+-- Change the highlight LinerNr
+-- vim.cmd([[highlight LineNr guifg='#F9E2AF']])
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- Mapea una tecla, por ejemplo <leader>v, para abrir una pestaña vertical
+
+function CloseTab()
+    vim.cmd(':bdelete')
+    vim.cmd(':bfirst')
+end
+
+-- Close Tab
+vim.api.nvim_set_keymap('n', '<C-w>', '<cmd>lua CloseTab()<CR>', { noremap = true, silent = true })
+
+-- Exit
+vim.api.nvim_set_keymap('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })
+
+-- Save
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+
+-- Change Tabs
+vim.api.nvim_set_keymap('n', 'L', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'H', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+
+-- Change to windows
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true, desc = 'Go to left window' })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true, desc = 'Go to lower window' })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true, desc = 'Go to upper window' })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true, desc = 'Go to right window' })
+
+-- Resize window using <ctrl> arrow keys
+
+vim.api.nvim_set_keymap('n', '<C-Up>', '<cmd>resize +2<cr>', { noremap = true, silent = true, desc = 'Increase window height' })
+vim.api.nvim_set_keymap('n', '<C-Down>', '<cmd>resize -2<cr>', { noremap = true, silent = true, desc = 'Decrease window height' })
+vim.api.nvim_set_keymap('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { noremap = true, silent = true, desc = 'Decrease window width' })
+vim.api.nvim_set_keymap('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { noremap = true, silent = true, desc = 'Increase window width' })
+
+
---
+++ lua/core/plugins.lua
@@ -0,0 +1,276 @@
+require('lazy').setup({
+    -- Plenary
+    "nvim-lua/plenary.nvim",
+
+    -- NeoTree
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+
+    'nvim-tree/nvim-web-devicons',
+    'nvim-lualine/lualine.nvim',
+
+    -- catppuccin
+    {
+        "catppuccin/nvim", name = "catppuccin", priority = 1000
+    },
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    'nvim-treesitter/nvim-treesitter',
+
+    -- cmp
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    'saadparwaiz1/cmp_luasnip',
+
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+    },
+    "rafamadriz/friendly-snippets",
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons'
+    },
+    'terrortylor/nvim-comment',
+    {
+        'nvim-telescope/telescope.nvim',
+        version = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+
+    {
+        'goolord/alpha-nvim',
+        config = function()
+            local alpha = require 'alpha'
+            local dashboard = require 'alpha.themes.dashboard'
+            dashboard.section.header.val = {
+                [[  ██╗ ██╗    ███████╗ █████╗ ███╗   ██╗████████╗████████╗██╗ █████╗  ██████╗     ██╗ ██╗  ]],
+                [[ ██╔╝██╔╝    ██╔════╝██╔══██╗████╗  ██║╚══██╔══╝╚══██╔══╝██║██╔══██╗██╔════╝     ╚██╗╚██╗ ]],
+                [[██╔╝██╔╝     ███████╗███████║██╔██╗ ██║   ██║      ██║   ██║███████║██║  ███╗     ╚██╗╚██╗]],
+                [[╚██╗╚██╗     ╚════██║██╔══██║██║╚██╗██║   ██║      ██║   ██║██╔══██║██║   ██║     ██╔╝██╔╝]],
+                [[ ╚██╗╚██╗    ███████║██║  ██║██║ ╚████║   ██║      ██║   ██║██║  ██║╚██████╔╝    ██╔╝██╔╝ ]],
+                [[  ╚═╝ ╚═╝    ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═╝ ╚═════╝     ╚═╝ ╚═╝  ]],
+
+            }
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("SPC f f", "󰈞  Find File", ":Telescope fd<CR>"),
+                dashboard.button("SPC f r", "󰈢  Recently Opened Files", ":Telescope oldfiles<CR>"),
+                dashboard.button("SPC f g", "󰷊  Find Word", ":Telescope live_grep<CR>"),
+                dashboard.button("SPC f h", "󰡯  Help Tags", ":Telescope help_tags<CR>"),
+                dashboard.button("q", "󰅚  Quit", ":qa<CR>"),
+            }
+            -- local handle = io.popen('fortune')
+            -- local fortune = handle:read("*a")
+            -- handle:close()
+            -- dashboard.section.footer.val = fortune
+
+            dashboard.config.opts.noautocmd = true
+
+            vim.cmd [[autocmd User AlphaReady echo 'ready']]
+
+            alpha.setup(dashboard.config)
+        end
+    },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end
+    },
+
+    'onsails/lspkind-nvim',
+
+    -- Trouble
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    -- Notify
+    'rcarriga/nvim-notify',
+
+    -- Nui
+    'MunifTanjim/nui.nvim',
+    -- Cmd Noice
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        }
+    },
+
+    -- NeoDev
+    {
+        "folke/neodev.nvim", opts = {}
+    },
+
+    -- Mini
+    {
+        'echasnovski/mini.pairs', version = '*'
+    },
+    {
+        'echasnovski/mini.surround', version = '*'
+    },
+    {
+        'echasnovski/mini.cursorword', version = '*'
+    },
+    {
+        'echasnovski/mini.indentscope',
+        version = '*',
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+        end,
+
+    },
+    {
+        'echasnovski/mini.indentscope', version = '*'
+    },
+
+    -- colorizer - NvChad
+    'NvChad/nvim-colorizer.lua',
+
+    -- indent-blankline
+    {
+        "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}
+    },
+
+    -- tailwind-colorizer
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        -- optionally, override the default options:
+        config = function()
+            require("tailwindcss-colorizer-cmp").setup({
+                color_square_width = 2,
+            })
+        end
+    },
+
+    -- The best plugin ever
+    'eandrju/cellular-automaton.nvim',
+
+    -- flash
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
+    },
+
+    -- gitSign
+    "lewis6991/gitsigns.nvim",
+
+    -- treeSJ
+    {
+        'Wansmer/treesj',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        config = function()
+            require('treesj').setup({
+                use_default_keymaps = false,
+                --[[ your config ]]
+            })
+        end,
+    },
+
+    -- SmoothCursor
+    {
+        'gen740/SmoothCursor.nvim',
+    },
+
+    -- BigFile
+    "LunarVim/bigfile.nvim",
+
+    -- Tabnine
+    { 'codota/tabnine-nvim', build = "./dl_binaries.sh" },
+
+    'mfussenegger/nvim-dap',
+
+    -- fancy UI for the debugger
+    {
+        "rcarriga/nvim-dap-ui",
+        -- stylua: ignore
+        keys = {
+            { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+            { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
+        },
+        opts = {},
+        config = function(_, opts)
+            -- setup dap config by VsCode launch.json file
+            -- require("dap.ext.vscode").load_launchjs()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup(opts)
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open({})
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close({})
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close({})
+            end
+        end,
+    },
+
+    -- virtual text for the debugger
+    {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+    },
+})
